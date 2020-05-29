a0841bd74c7556ea7b520f6adb6b31527305418d
azspider <aalokjha59@gmail.com>
2020-05-29 10:14:47 +0545

    Trained and Build the model

---
+++ logistic_regression.py
@@ -0,0 +1,32 @@
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+# Importing the dataset
+dataset = pd.read_csv('Forest_fire.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+pickle.dump(classifier,open('model.pkl','wb'))
+model = pickle.load(open('model.pkl','rb'))
